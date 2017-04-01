2d4176909bc311b3b3e07e996ad4af8f3c88e6c9
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-04-01 14:53:40 +0200

    Add a compiler that accepts anything

--- apps/compiler/Main.hs
+++ apps/compiler/Main.hs
@@ -1,4 +1,12 @@
 module Main ( main ) where
 
+import System.IO
+
 main :: IO ()
-main = undefined
+main = putStrLnStderr "OK"
+
+putStrStderr :: String -> IO ()
+putStrStderr = hPutStr stderr
+
+putStrLnStderr :: String -> IO ()
+putStrLnStderr = hPutStrLn stderr
