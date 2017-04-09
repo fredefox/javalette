76db880815f47f7f2dcf81c675e91d36ecb8da50
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-04-09 16:36:02 +0200

    Define some types for the LLVM compiler

--- src/Javalette/Backend/LLVM.hs
+++ src/Javalette/Backend/LLVM.hs
@@ -2,7 +2,43 @@ module Javalette.Backend.LLVM
   ( backend
   ) where
 
-import Javalette.Backend
+import Control.Monad.Except
+import Control.Monad.State
+
+import Javalette.Backend.Internals
+import qualified Javalette.Syntax as Jlt
+import qualified Javalette.Backend.LLVM.Language as LLVM
+import Javalette.PrettyPrint
 
 backend :: Backend
-backend = error "Unimplemented"
+backend = Backend
+  { compiler = compile
+  }
+
+compile :: Jlt.Prog -> IO ()
+compile = runCompiler . compileProg
+
+data CompilerErr
+data Env = Env
+
+initEnv :: Env
+initEnv = Env
+
+instance Pretty CompilerErr where
+  pPrint = error "Unimplemented"
+
+type Compiler a = StateT Env (ExceptT CompilerErr LLVM.Program) a
+
+runCompiler :: Compiler a -> IO ()
+runCompiler = handleErrors . runExceptT . (`execStateT` initEnv)
+
+handleErrors :: Pretty e => IO (Either e a) -> IO ()
+handleErrors act = act >>= printErr
+
+printErr :: Pretty e => Either e a -> IO ()
+printErr e = case e of
+  Left err -> prettyPrint err
+  Right{}  -> return ()
+
+compileProg :: Jlt.Prog -> Compiler ()
+compileProg = undefined
